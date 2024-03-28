de0b1819e14ad9d64542eaefb6585903aed2e573
ayeshawaheed85 <ayeshaw686@gmail.com>
2024-03-28 13:47:32 +0500

    "Simple CLI Calculator using typescript"

---
+++ README.md
@@ -0,0 +1,8 @@
+#Description
+    Simple CLI Calculator using TypeScript and Node.js
+    This is a simple Command Line Interface (CLI) calculator application built using TypeScript and Node.js. It allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division from the command line.
+
+#Features
+    -Supports addition, subtraction, multiplication, and division.
+    -Simple and easy-to-use command line interface.
+    -Written in TypeScript for better code maintainability and type safety.
---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+async function calculator() {
+    let answers = await inquirer.prompt([
+        {
+            type: "number",
+            name: "num1",
+            message: "Please enter first Number: "
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Please enter Second Number: "
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["*", "+", "-", "/"],
+            message: "Please select operator: "
+        },
+    ]);
+    let { num1, num2, operator } = answers;
+    if (num1 !== undefined && num2 !== undefined && operator !== undefined) {
+        let result = 0;
+        if (operator === "+") {
+            result = num1 + num2;
+        }
+        else if (operator === "-") {
+            result = num1 - num2;
+        }
+        else if (operator === "*") {
+            result = num1 * num2;
+        }
+        else if (operator === "/") {
+            if (num2 !== 0) {
+                result = num1 / num2;
+            }
+            else {
+                console.log("Error: Division by zero");
+                return;
+            }
+        }
+        console.log("Your answer is:", result);
+    }
+    else {
+        console.log("Error: Insufficient inputs");
+    }
+}
+calculator();
